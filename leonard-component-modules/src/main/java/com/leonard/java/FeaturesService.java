2fe46f45047a6cc87b580e50f8b8d0978a244cfe
wanghc3 <wanghc3@asiainfo.com>
2021-05-28 17:35:12 +0800

    doc

---
+++ leonard-component-modules/src/main/java/com/leonard/java/FeaturesService.java
@@ -0,0 +1,15 @@
+package com.leonard.java;
+
+/**
+ * @author wanghc3
+ * @description JDK 1.8 特性
+ * @date 2021/5/28 9:42
+ */
+public class FeaturesService {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
